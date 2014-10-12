7b390eb3cb9b2a6598902c82de0ca820b71cfea0
Andrew El-Masry <andrewelmasrye@gmail.com>
2014-10-12 11:53:36 -0400

    Moving on to Repositories

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,13 @@
-var sideBarTemplate = $('#requiresUserApi').html();
+var topTemplate = $('#topUserApi').html();
+var showTop = _.template(topTemplate);
+var userUrl='https://api.github.com/users/elmasrya';
+
+$.getJSON(userUrl).done(
+  function (user) {
+    $('.top').append( showTop(user) );
+});
+
+var sideBarTemplate = $('#sideBarUserApi').html();
 var showSideBar = _.template(sideBarTemplate);
 var userUrl='https://api.github.com/users/elmasrya';
 
@@ -7,7 +16,7 @@ $.getJSON(userUrl).done(
     $('.sideBar').append( showSideBar(user) );
 });
 
-
+/*
 var orgsTemplate=$('#requiresOrgsApi').html();
 var showOrgs= _.template(orgsTemplate);
 var orgsUrl= 'https://api.github.com/users/elmasrya/orgs';
@@ -16,3 +25,4 @@ $.getJSON(orgsUrl).done (
 	function(user) {
 	$('.orgsBox').append( showOrgs(user) );
 });
+*/
