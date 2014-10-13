11ef43a52d0fa8d4ba8ab1c7fb97ac58678bfdc2
Andrew El-Masry <andrewelmasry@gmail.com>
2014-10-13 06:38:10 -0400

    finished with header

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,7 @@
+/*............Top of webpage.....................*/
+/*................................................*/
+
+
 var topTemplate = $('#topUserApi').html();
 var showTop = _.template(topTemplate);
 var userUrl='https://api.github.com/users/elmasrya';
@@ -7,13 +11,75 @@ $.getJSON(userUrl).done(
     $('.top').append( showTop(user) );
 });
 
+/*............Top of sidebar.....................*/
+/*................................................*/
 
 
-
-var sideBarTemplate = $('#sideBarUserApi').html();
-var showSideBar = _.template(sideBarTemplate);
+var userTemplate = $('#userProfileApi').html();
+var showUser = _.template(userTemplate);
 
 $.getJSON(userUrl).done(
   function (user) {
-    $('.sideBar').append( showSideBar(user) );
+    $('.profileUser').append( showUser(user) );
+});
+
+
+/*............middle of sidebar.....................*/
+/*................................................*/
+
+var userInfoTemplate = $('#userInfoApi').html();
+var showUserInfo = _.template(userInfoTemplate);
+
+$.getJSON(userUrl).done(
+  function (user) {
+    $('.myInfo').append( showUserInfo(user) );
+});
+
+/*..............Followers, Starred, Following.....................*/
+/*................................................*/
+
+var userSocialTemplate = $('#userSocialApi').html();
+var showUserSocial = _.template(userSocialTemplate);
+
+$.getJSON(userUrl).done(
+  function (user) {
+    $('.count1').append( showUserSocial(user) );
+});
+
+
+var starredTemplate = $('#starredApi').html();
+var showStarred = _.template(starredTemplate);
+var starredUrl='https://api.github.com/users/elmasrya/starred';
+
+$.getJSON(starredUrl).done(
+  function (user) {
+    user.forEach( function(x) {
+    $('.count2').append( showStarred(x));
+  });
+
+});
+
+
+var userSocialTemplate2 = $('#userSocialApi2').html();
+var showUserSocial2 = _.template(userSocialTemplate2);
+
+$.getJSON(userUrl).done(
+  function (user) {
+    $('.count3').append( showUserSocial2(user) );
+});
+
+/*............bottom of sidebar.....................*/
+/*................................................*/
+
+
+var orgsTemplate = $('#orgsApi').html();
+var showOrgs = _.template(orgsTemplate);
+var orgsUrl='https://api.github.com/users/elmasrya/orgs';
+
+$.getJSON(orgsUrl).done(
+  function (user) {
+    user.forEach( function(x) {
+    $('.orgsBox').append( showOrgs(x));
+  });
+
 });
