670562583e56dcac41051e4f979871bd96b0e433
Andrew El-Masry <andrewelmasry@gmail.com>
2014-10-12 20:32:02 -0400

    foreach loop

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,22 +7,13 @@ $.getJSON(userUrl).done(
     $('.top').append( showTop(user) );
 });
 
+
+
+
 var sideBarTemplate = $('#sideBarUserApi').html();
 var showSideBar = _.template(sideBarTemplate);
-var userUrl='https://api.github.com/users/elmasrya';
 
 $.getJSON(userUrl).done(
   function (user) {
     $('.sideBar').append( showSideBar(user) );
 });
-
-/*
-var orgsTemplate=$('#requiresOrgsApi').html();
-var showOrgs= _.template(orgsTemplate);
-var orgsUrl= 'https://api.github.com/users/elmasrya/orgs';
-
-$.getJSON(orgsUrl).done (
-	function(user) {
-	$('.orgsBox').append( showOrgs(user) );
-});
-*/
